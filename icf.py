correct the normalization approach and implement it in all relevant files, rendering renormalization.ipynb obsolete

icf.py
@@ -8,8 +8,6 @@ from nnscf import NNSCFMapper, normalize, denormalize
 import os
 import time
 
-# todo: reconsider whether the normalization is correctly implemented here and in other scripts
-
 start = time.time()
 
 # Parameters
@@ -25,7 +23,7 @@ cr = 10 ** (cr_dB / 20)
 iterations = 3
 iterations_str = f"{iterations} iteration{"s" if iterations > 1 else ""}"
 
-# modulation scheme
+# Modulation scheme
 mod = "16qam"
 # mod = "qpsk"
 if mod == "16qam":
@@ -34,22 +32,25 @@ elif mod == "qpsk":
     M = 4
 modulate, _ = get_modem(M)
 
-# clipping technique
+# Clipping technique
 # tech = "icf"
 tech = "scf"
 
 # Hyperparameters (used only in saving and loading files, not in the actual C&F process)
-lr = 0.001
-# lr = 0.01
-lr_str = "dot" + str(lr).split(".")[1]
+# lr = 0.001
+lr = 0.1
+lr_list = str(float(lr)).split(".")
+lr_str = f"dot{lr_list[1]}" if lr < 1 else f"{lr_list[0]}dot{lr_list[1]}"
+
+# Optimizer
+# opt = "Adam"
+opt = "LBFGS"
 
 # Data splitting (used only in saving and loading files, not in the actual C&F process)
-datasize = 100
-# datasize = 120
+data_size = 100
 train_size = 70
-# val_size = 20
 val_size = 10
-test_size = datasize - train_size - val_size
+test_size = data_size - train_size - val_size
 if test_size:
     one_batch = None
 else:
@@ -57,31 +58,47 @@ else:
     one_batch = "00"
 train_val_test_str = f"{train_size}_{val_size}_{test_size}" if val_size else f"{train_size}_{test_size}"
 
-# Optimizer
-opt = "Adam"
-# opt = "LBFGS"
-
 params = f"{opt} optimizer {tech.upper()} {mod.upper()} (N={N}, L={L}, CR={cr_dB}dB)\nlr = {lr} ({train_size} Training/{val_size} Validation/{test_size} Testing)"
 
-# model_dir = "./trained_models/"
-model_dir = "./new architecture/"
+relev = "./relevant files/"
+os.makedirs(relev, exist_ok=True)
 
 # * Load nnscf model
 
 # 2. Load the trained models
 # Check for GPU availability and set device accordingly
 device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
+
+checkpoint_re = torch.load(
+    os.path.join(relev, f"{opt}_{mod}_{tech}_{train_val_test_str}_{lr_str}_weights_limits_re.pth"),
+    weights_only=False
+)
+checkpoint_im = torch.load(
+    os.path.join(relev, f"{opt}_{mod}_{tech}_{train_val_test_str}_{lr_str}_weights_limits_im.pth"),
+    weights_only=False
+)
+
 # Instantiate the empty models
 NN_Mod_Re = NNSCFMapper().to(device)
 NN_Mod_Im = NNSCFMapper().to(device)
 
 # Inject the trained weights
-NN_Mod_Re.load_state_dict(torch.load(os.path.join(model_dir, f"{opt}_{mod}_{tech}_mod_re_weights_{train_val_test_str}_{lr_str}.pth"), weights_only=True))
-NN_Mod_Im.load_state_dict(torch.load(os.path.join(model_dir, f"{opt}_{mod}_{tech}_mod_im_weights_{train_val_test_str}_{lr_str}.pth"), weights_only=True))
+NN_Mod_Re.load_state_dict(checkpoint_re['model_state_dict'])
+NN_Mod_Im.load_state_dict(checkpoint_im['model_state_dict'])
 
 NN_Mod_Re.eval()
 NN_Mod_Im.eval()
 
+# 2. Grab the saved normalization limits
+X_r_min = checkpoint_re['X_min']
+X_r_max = checkpoint_re['X_max']
+Y_r_min = checkpoint_re['Y_min']
+Y_r_max = checkpoint_re['Y_max']
+
+X_i_min = checkpoint_im['X_min']
+X_i_max = checkpoint_im['X_max']
+Y_i_min = checkpoint_im['Y_min']
+Y_i_max = checkpoint_im['Y_max']
 
 # Simulation
 # unclipped_papr, pred_papr, icf_papr = [], [], [[] for _ in range(iterations)]
@@ -146,8 +163,8 @@ middle = time.time()
 # * Compare with nnscf model
 
 # 1. Normalize the data and turn it to torch tensors
-tx_real, tx_minmax_real = normalize(tx_time[0])
-tx_imag, tx_minmax_imag = normalize(tx_time[1])
+tx_real = normalize(tx_time[0], (X_r_min, X_r_max))
+tx_imag = normalize(tx_time[1], (X_i_min, X_i_max))
 
 
 # 3. Generate Predictions (No gradients needed for testing)
@@ -160,8 +177,8 @@ with torch.no_grad():
     predicted_imag = NN_Mod_Im(X_imag_flat).view_as(tx_imag).cpu().numpy()
 
 
-pred_denorm_real = denormalize(predicted_real, tx_minmax_real)
-pred_denorm_imag = denormalize(predicted_imag, tx_minmax_imag)
+pred_denorm_real = denormalize(predicted_real, (Y_r_min, Y_r_max))
+pred_denorm_imag = denormalize(predicted_imag, (Y_i_min, Y_i_max))
 
 
 # 4. Reconstruct the Complex OFDM Signals
@@ -183,18 +200,18 @@ labels = ['Original', f'ICF ({iterations_str})', f'NN{tech.upper()} Predicted']
 # papr_list = [unclipped_papr, icf_papr[-1], pred_papr]
 cm_list = [unclipped_cm, icf_cm[-1], pred_cm]
 
-# plot_ccdf_compare(papr_list, f"Original vs {tech.upper()} vs {title}", labels)
-plot_ccdf_compare(cm_list, f"Original vs {tech.upper()} vs {title}", labels, metric="CM")
+# plot_ccdf_compare(papr_list, f"Original vs ICF vs {title}", labels)
+plot_ccdf_compare(cm_list, f"Original vs ICF vs {title}", labels, metric="CM")
 
 # fixme: both percentile and max are not the most efficient solutions
 
 # todo: find a way to embed the floor part into the plotting function
 # 1. Define the target y-levels (probabilities)
-papr_target_y = 1e-4
+# papr_target_y = 1e-4
 cm_target_y = 1e-3
 
 # 2. Convert CCDF y-level to a standard percentile (e.g., 1e-4 becomes 99.99)
-papr_percentile = (1.0 - papr_target_y) * 100.0
+# papr_percentile = (1.0 - papr_target_y) * 100.0
 cm_percentile = (1.0 - cm_target_y) * 100.0
 
 # 3. Extract the exact x-axis values (PAPR/CM) where the line cuts the graph
@@ -233,8 +250,9 @@ print("time taken in the icf calculations:", icf_time)
 print("time taken in outer samples_per_L loop:", samples_per_L_minus_icf_time)
 
 labels = ['Original', f'Clipped ({iterations_str})', f'ICF ({iterations_str})', f'NN{tech.upper()} Predicted']
+signals_image_path = os.path.join(relev, f"{opt}_{mod}_{tech}_{train_val_test_str}_{lr_str}_signals.png")
 # plot_signals(signals, labels, A)
-plot_signals2(signals, labels, A)
+plot_signals2(signals, labels, A, signals_image_path)
 
 signals_re = {}
 signals_im = {}

icfber.py
@@ -8,8 +8,6 @@ from nnscf import NNSCFMapper, normalize, denormalize
 import os
 import time
 
-# todo: reconsider whether the normalization is correctly implemented here and in other scripts
-
 start = time.time()
 
 # Parameters
@@ -24,7 +22,7 @@ cr = 10 ** (cr_dB / 20)
 iterations = 3
 iterations_str = f"{iterations} iteration{"s" if iterations > 1 else ""}"
 
-# modulation scheme
+# Modulation scheme
 mod = "16qam"
 # mod = "qpsk"
 # edits the stops to be same as the paper, and declares M for the modem
@@ -36,22 +34,25 @@ elif mod == "qpsk":
     stop = 8
 modulate, demodulate = get_modem(M)
 
-# clipping technique
+# Clipping technique
 # tech = "icf"
 tech = "scf"
 
 # Hyperparameters (used only in saving and loading files, not in the actual C&F process)
-lr = 0.001
-# lr = 0.01
-lr_str = "dot" + str(lr).split(".")[1]
+# lr = 0.001
+lr = 0.1
+lr_list = str(float(lr)).split(".")
+lr_str = f"dot{lr_list[1]}" if lr < 1 else f"{lr_list[0]}dot{lr_list[1]}"
+
+# Optimizer
+# opt = "Adam"
+opt = "LBFGS"
 
 # Data splitting (used only in saving and loading files, not in the actual C&F process)
-datasize = 100
-# datasize = 120
+data_size = 100
 train_size = 70
-# val_size = 20
 val_size = 10
-test_size = datasize - train_size - val_size
+test_size = data_size - train_size - val_size
 if test_size:
     one_batch = None
 else:
@@ -59,30 +60,47 @@ else:
     one_batch = "00"
 train_val_test_str = f"{train_size}_{val_size}_{test_size}" if val_size else f"{train_size}_{test_size}"
 
-# Optimizer
-opt = "Adam"
-# opt = "LBFGS"
-
 params = f"{opt} optimizer {tech.upper()} {mod.upper()} (N={N}, L={L}, CR={cr_dB}dB)\nlr = {lr} ({train_size} Training/{val_size} Validation/{test_size} Testing)"
 
-# model_dir = "./trained_models/"
-model_dir = "./new architecture/"
+relev = "./relevant files/"
+os.makedirs(relev, exist_ok=True)
 
 # * Load nnscf model
 
 # 2. Load the trained models
 # Check for GPU availability and set device accordingly
 device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
+
+checkpoint_re = torch.load(
+    os.path.join(relev, f"{opt}_{mod}_{tech}_weights_limits_re_{train_val_test_str}_{lr_str}.pth"),
+    weights_only=False
+)
+checkpoint_im = torch.load(
+    os.path.join(relev, f"{opt}_{mod}_{tech}_weights_limits_im_{train_val_test_str}_{lr_str}.pth"),
+    weights_only=False
+)
+
 # Instantiate the empty models
 NN_Mod_Re = NNSCFMapper().to(device)
 NN_Mod_Im = NNSCFMapper().to(device)
 
 # Inject the trained weights
-NN_Mod_Re.load_state_dict(torch.load(os.path.join(model_dir, f"{opt}_{mod}_{tech}_mod_re_weights_{train_val_test_str}_{lr_str}.pth"), weights_only=True))
-NN_Mod_Im.load_state_dict(torch.load(os.path.join(model_dir, f"{opt}_{mod}_{tech}_mod_im_weights_{train_val_test_str}_{lr_str}.pth"), weights_only=True))
+NN_Mod_Re.load_state_dict(checkpoint_re['model_state_dict'])
+NN_Mod_Im.load_state_dict(checkpoint_im['model_state_dict'])
 
 NN_Mod_Re.eval()
 NN_Mod_Im.eval()
+
+# 2. Grab the saved normalization limits
+X_r_min = checkpoint_re['X_min']
+X_r_max = checkpoint_re['X_max']
+Y_r_min = checkpoint_re['Y_min']
+Y_r_max = checkpoint_re['Y_max']
+
+X_i_min = checkpoint_im['X_min']
+X_i_max = checkpoint_im['X_max']
+Y_i_min = checkpoint_im['Y_min']
+Y_i_max = checkpoint_im['Y_max']
 
 # ber_theory_list = []
 BER_results = {
@@ -100,11 +118,12 @@ num_symb_minus_icf_nn_time = 0
 icf_time = 0
 nn_time = 0
 
-# todo: try to understand whether to downsample before awgn or not
+constel_image_path = os.path.join(relev, f"{opt}_{mod}_{tech}_{train_val_test_str}_{lr_str}_")
 
 EbNo_range = np.arange(0, stop + 1, 1) # does not affect ccdf
 bits_per_symbol = int(np.log2(M))
 num_symb = 100
+
 
 tx_ofdm_no_clip = None
 rx_symbols_no_clip = None
@@ -140,17 +159,15 @@ for EbNo_dB in EbNo_range:
 
         if EbNo_dB == EbNo_range[0] and not plotted:
             limit = np.max(np.abs(tx_symbols)) * 1.1
-            plot_constellation(tx_symbols, mod, limit, title=f'Transmitted Constellation at E$_b$/N$_0$ = {EbNo_dB}dB', label='Tx Symbols', color='black')
+            plot_constellation(tx_symbols, mod, limit, title=f'Transmitted Constellation at E$_b$/N$_0$ = {EbNo_dB}dB', label='Tx Symbols', color='black', save=(constel_image_path+"Tx.png"))
 
         # No clipping case
         tx_ofdm_no_clip = np.fft.ifft(tx_symbols)
 
-        # ? how does it differ
         rx_symbols_no_clip = emulate_awgn_channel(tx_ofdm_no_clip, CP, SNR_dB)
-        # rx_symbols_no_clip = emulate_awgn_channel(tx_ofdm_no_clip, CP, SNR_dB, L)
 
         if (EbNo_dB == EbNo_range[0] or EbNo_dB == EbNo_range[-1]) and not plotted:
-            plot_constellation(rx_symbols_no_clip, mod, limit, title=f'Received Constellation at E$_b$/N$_0$ = {EbNo_dB}dB (No Clip)', label='Rx Symbols (No Clip)', color='black')
+            plot_constellation(rx_symbols_no_clip, mod, limit, title=f'Received Constellation at E$_b$/N$_0$ = {EbNo_dB}dB (No Clip)', label='Rx Symbols (No Clip)', color='black', save=(constel_image_path+f"Rx_no_clip_{EbNo_dB}db.png"))
 
         rx_bits_no_clip = demodulate(rx_symbols_no_clip, bits=True)
 
@@ -168,16 +185,12 @@ for EbNo_dB in EbNo_range:
         for i in range(1, iterations + 1):
             filtered_time_icf, clipped_time_icf, _ = clip_and_filter_time(filtered_time_icf, cr, N)
 
-            # ? how does it differ
-            # rx_symbols_clipped_icf = emulate_awgn_channel(clipped_time_icf, CP, SNR_dB)
             rx_symbols_clipped_icf = emulate_awgn_channel(clipped_time_icf, CP, SNR_dB, L)
 
             rx_bits_clipped_icf = demodulate(rx_symbols_clipped_icf, bits=True)
 
             bit_error_clipped_icf[i] += np.sum(tx_bits != rx_bits_clipped_icf)
 
-            # ? how does it differ
-            # rx_symbols_icf = emulate_awgn_channel(filtered_time_icf, CP, SNR_dB)
             rx_symbols_icf = emulate_awgn_channel(filtered_time_icf, CP, SNR_dB, L)
 
             rx_bits_icf = demodulate(rx_symbols_icf, bits=True)
@@ -185,11 +198,11 @@ for EbNo_dB in EbNo_range:
             bit_error_icf[i] += np.sum(tx_bits != rx_bits_icf)
 
             # if (EbNo_dB == EbNo_range[0] or EbNo_dB == EbNo_range[-1]) and not plotted:
-            #     plot_constellation(rx_symbols_clipped_icf, mod, limit, title=f'Received Constellation at E$_b$/N$_0$ = {EbNo_dB}dB (Clipped)', label='Rx Symbols (Clipped)', color='magenta')
-            #     plot_constellation(rx_symbols_icf, mod, limit, title=f'Received Constellation at E$_b$/N$_0$ = {EbNo_dB}dB (Filtered)', label='Rx Symbols (Filtered)', color='red')
+            #     plot_constellation(rx_symbols_clipped_icf, mod, limit, title=f'Received Constellation at E$_b$/N$_0$ = {EbNo_dB}dB (Clipped)', label='Rx Symbols (Clipped)', color='magenta', save=(constel_image_path+f"Rx_clipped_icf_{EbNo_dB}db.png"))
+            #     plot_constellation(rx_symbols_icf, mod, limit, title=f'Received Constellation at E$_b$/N$_0$ = {EbNo_dB}dB (Filtered)', label='Rx Symbols (Filtered)', color='red', , save=(constel_image_path+f"Rx_icf_{EbNo_dB}db.png"))
 
         if (EbNo_dB == EbNo_range[0] or EbNo_dB == EbNo_range[-1]) and not plotted:
-            plot_constellation(rx_symbols_icf, mod, limit, title=f'Received Constellation at E$_b$/N$_0$ = {EbNo_dB}dB (Filtered)', label='Rx Symbols (Filtered)', color='red')
+            plot_constellation(rx_symbols_icf, mod, limit, title=f'Received Constellation at E$_b$/N$_0$ = {EbNo_dB}dB (Filtered)', label='Rx Symbols (Filtered)', color='red', save=(constel_image_path+f"Rx_icf_{EbNo_dB}db.png"))
 
         t5 = time.time()
         icf_time += t5 - t4
@@ -198,8 +211,8 @@ for EbNo_dB in EbNo_range:
 
         # 1. Normalize the data and turn it to torch tensors
         # ? why tx_time_oversampled
-        tx_real, minmax_real = normalize(tx_time_oversampled.real)
-        tx_imag, minmax_imag = normalize(tx_time_oversampled.imag)
+        tx_real = normalize(tx_time_oversampled.real, (X_r_min, X_r_max))
+        tx_imag = normalize(tx_time_oversampled.imag, (X_i_min, X_i_max))
 
         # 2. Generate Predictions
         with torch.no_grad():
@@ -211,26 +224,15 @@ for EbNo_dB in EbNo_range:
             predicted_real = NN_Mod_Re(X_real_flat).view_as(tx_real).cpu().numpy()
             predicted_imag = NN_Mod_Im(X_imag_flat).view_as(tx_imag).cpu().numpy()
 
-            # # Pass the original input straight through
-            # predicted_real = tx_real_tensor.squeeze(0).cpu().numpy().flatten()
-            # predicted_imag = tx_imag_tensor.squeeze(0).cpu().numpy().flatten()
-            # # plt.plot(tx_real_tensor.squeeze().cpu().numpy(), label="Original Input (Normalized)")
-            # # plt.plot(predicted_real, label="NN Output (Flatlined)")
-            # # plt.legend()
-            # # plt.show()
-            # # exit()
-
-        pred_denorm_real = denormalize(predicted_real, minmax_real)
-        pred_denorm_imag = denormalize(predicted_imag, minmax_imag)
+        pred_denorm_real = denormalize(predicted_real, (Y_r_min, Y_r_max))
+        pred_denorm_imag = denormalize(predicted_imag, (Y_i_min, Y_i_max))
 
         predicted_complex = pred_denorm_real + 1j * pred_denorm_imag
 
-        # ? how does it differ
-        # predicted_symbols = emulate_awgn_channel(predicted_complex, CP, SNR_dB)
         predicted_symbols = emulate_awgn_channel(predicted_complex, CP, SNR_dB, L)
 
         if (EbNo_dB == EbNo_range[0] or EbNo_dB == EbNo_range[-1]) and not plotted:
-            plot_constellation(predicted_symbols, mod, limit, title=f'Predicted Constellation at E$_b$/N$_0$ = {EbNo_dB}dB', label='Rx Symbols (Predicted)', color='green')
+            plot_constellation(predicted_symbols, mod, limit, title=f'Predicted Constellation at E$_b$/N$_0$ = {EbNo_dB}dB', label='Rx Symbols (Predicted)', color='green', save=(constel_image_path+f"Rx_pred_{EbNo_dB}db.png"))
         plotted = True
 
         predicted_bits = demodulate(predicted_symbols, bits=True)
@@ -251,9 +253,9 @@ for EbNo_dB in EbNo_range:
     BER_results['predicted'].append(ber_predicted)
 
     for i in range(1, iterations + 1):
-        ber_clipped = bit_error_clipped_icf[i] / total_bits
+        ber_clipped_icf = bit_error_clipped_icf[i] / total_bits
         ber_icf = bit_error_icf[i] / total_bits
-        BER_results[f'clipped icf ({f"{i} iteration{"s" if i > 1 else ""}"})'].append(ber_clipped)
+        BER_results[f'clipped icf ({f"{i} iteration{"s" if i > 1 else ""}"})'].append(ber_clipped_icf)
         BER_results[f'icf ({f"{i} iteration{"s" if i > 1 else ""}"})'].append(ber_icf)
     # ber_theory_list.append(ber_theory)
 

icfdata.py
@@ -7,6 +7,8 @@ from nnscf import normalize
 import os
 import time
 
+# todo: see a way to add ber to the data so that we can use it in the loss function
+
 start = time.time()
 
 # Parameters
@@ -21,7 +23,7 @@ cr_dB = 6
 cr = 10 ** (cr_dB / 20)
 iterations = 3
 
-# modulation scheme
+# Modulation scheme
 mod = "16qam"
 # mod = "qpsk"
 if mod == "16qam":
@@ -31,23 +33,33 @@ elif mod == "qpsk":
 modulate, _ = get_modem(M)
 
 # the actual size of the dataset
-datasize = 100
-# datasize = 120
+data_size = 100
 
 params = f"ICF {mod.upper()} (N={N}, L={L}, CR={cr_dB}dB)"
 
-# todo: see a way to add ber to the nnicf data so that we can use it in the loss function
+raw_dir = "./raw dir/"
+relev = "./relevant files/"
+os.makedirs(raw_dir, exist_ok=True)
+os.makedirs(relev, exist_ok=True)
 
-pt_dir = "./pt_dir/"
-os.makedirs(pt_dir, exist_ok=True)
-
-datasize_minus_samples_per_L_loop = 0
+data_size_minus_samples_per_L_loop = 0
 samples_per_L_minus_icf_time = 0
 icf_time = 0
 
+raw_limits = {
+    'X_r_min': [],
+    'X_r_max': [],
+    'Y_r_min': [],
+    'Y_r_max': [],
+    'X_i_min': [],
+    'X_i_max': [],
+    'Y_i_min': [],
+    'Y_i_max': []
+}
+
 # tx, rx = [], []
 before_loop = time.time()
-for i in range(datasize):
+for i in range(data_size):
     loop_start = time.time() if i else before_loop
     tx_time, rx_time = [[], []], [[], []]
     for _ in range(samples_per_L):
@@ -66,16 +78,16 @@ for i in range(datasize):
         samples_per_L_minus_icf_time += t2 - t1
 
         # Process C&F
-        x_filt = x_time.copy()
+        x_icf = x_time.copy()
         for j in range(iterations):
-            x_filt, _, _ = clip_and_filter_time(x_filt, cr, N)
+            x_icf, _, _ = clip_and_filter_time(x_icf, cr, N)
 
         t3 = time.time()
         icf_time += t3 - t2
 
         # store the final iteration's real and imag parts separately
-        rx_time[0].append(np.real(x_filt).astype(np.float32, copy=False))
-        rx_time[1].append(np.imag(x_filt).astype(np.float32, copy=False))
+        rx_time[0].append(np.real(x_icf).astype(np.float32, copy=False))
+        rx_time[1].append(np.imag(x_icf).astype(np.float32, copy=False))
         t4 = time.time()
         samples_per_L_minus_icf_time += t4 - t3
     t5 = time.time()
@@ -83,49 +95,64 @@ for i in range(datasize):
     tx_time = np.array(tx_time, dtype=np.float32)
     rx_time = np.array(rx_time, dtype=np.float32)
 
-    # DIRECT IN-MEMORY NORMALIZATION & PYTORCH BUNDLING
-
-    # Extract Real and Imaginary arrays and extract exact physical limits
-    X_real_norm, (X_r_min, X_r_max) = normalize(tx_time[0])
-    X_imag_norm, (X_i_min, X_i_max) = normalize(tx_time[1])
-    Y_real_norm, (Y_r_min, Y_r_max) = normalize(rx_time[0])
-    Y_imag_norm, (Y_i_min, Y_i_max) = normalize(rx_time[1])
-
     # Save Real File as PyTorch Dictionary
+    # ! turn into tensors to avoid weights_only error
     torch.save({
-        'X_norm': X_real_norm.detach().clone().to(torch.float32),
-        'Y_norm': Y_real_norm.detach().clone().to(torch.float32),
-        'X_min': X_r_min, 'X_max': X_r_max,
-        'Y_min': Y_r_min, 'Y_max': Y_r_max
-    }, os.path.join(pt_dir, f"{mod}_icf_part_{i:03d}_real.pt"))
+        'X_raw': torch.tensor(tx_time[0], dtype=torch.float32),
+        'Y_raw': torch.tensor(rx_time[0], dtype=torch.float32)
+    }, os.path.join(raw_dir, f"{mod}_icf_part_{i:02d}_real.pt"))
 
     # Save Imaginary File as PyTorch Dictionary
     torch.save({
-        'X_norm': X_imag_norm.detach().clone().to(torch.float32),
-        'Y_norm': Y_imag_norm.detach().clone().to(torch.float32),
-        'X_min': X_i_min, 'X_max': X_i_max,
-        'Y_min': Y_i_min, 'Y_max': Y_i_max
-    }, os.path.join(pt_dir, f"{mod}_icf_part_{i:03d}_imag.pt"))
+        'X_raw': torch.tensor(tx_time[1], dtype=torch.float32),
+        'Y_raw': torch.tensor(rx_time[1], dtype=torch.float32)
+    }, os.path.join(raw_dir, f"{mod}_icf_part_{i:02d}_imag.pt"))
+
+    # Save raw data limits
+    raw_limits['X_r_min'].append(tx_time[0].min())
+    raw_limits['X_r_max'].append(tx_time[0].max())
+    raw_limits['Y_r_min'].append(rx_time[0].min())
+    raw_limits['Y_r_max'].append(rx_time[0].max())
+    raw_limits['X_i_min'].append(tx_time[1].min())
+    raw_limits['X_i_max'].append(tx_time[1].max())
+    raw_limits['Y_i_min'].append(rx_time[1].min())
+    raw_limits['Y_i_max'].append(rx_time[1].max())
 
     # Manually delete variables to free RAM for the next iteration
-    del tx_time, rx_time, X_real_norm, X_imag_norm, Y_real_norm, Y_imag_norm
+    del tx_time, rx_time
     # gc.collect()
 
     loop_end = time.time()
-    print(f"Iteration {i + 1}/{datasize} completed in {loop_end - loop_start:.2f} seconds")
+    print(f"Iteration {i + 1}/{data_size} completed in {loop_end - loop_start:.2f} seconds")
     print(f"{loop_end - before_loop:.2f} seconds passed since before loop start")
     t6 = time.time()
-    datasize_minus_samples_per_L_loop += t6 - t5
+    data_size_minus_samples_per_L_loop += t6 - t5
 
-title = f"Data Mapping\n{params}"
-plot_dataset_mapping(os.path.join(pt_dir, f"{mod}_icf_part_{0:03d}_real.pt"), "NNICF Real " + title)
-plot_dataset_mapping(os.path.join(pt_dir, f"{mod}_icf_part_{0:03d}_imag.pt"), "NNICF Imaginary " + title)
+torch.save(raw_limits, os.path.join(relev, f"{mod}_icf_{data_size}_raw_limits.pt"))
+
+t7 = time.time()
+data_size_minus_samples_per_L_loop += t7 - t6
+
+real_path = f"{mod}_icf_part_{0:02d}_real"
+imag_path = f"{mod}_icf_part_{0:02d}_imag"
+plot_dataset_mapping(os.path.join(raw_dir, real_path + ".pt"), f"NNICF Real\n{params} Batch #{0:02d}", raw=True, save=os.path.join(relev, real_path + ".png"))
+plot_dataset_mapping(os.path.join(raw_dir, imag_path + ".pt"), f"NNICF Imaginary\n{params} Batch #{0:02d}", raw=True, save=os.path.join(relev, real_path + ".png"))
+
+# x = torch.load(os.path.join(raw_dir, real_path + ".pt"), weights_only=True)
+# X = normalize(x['X_raw'], (min(raw_limits['X_r_min']), max(raw_limits['X_r_max'])))
+# Y = normalize(x['Y_raw'], (min(raw_limits['Y_r_min']), max(raw_limits['Y_r_max'])))
+# torch.save({
+#     "X_norm": X,
+#     "Y_norm": Y
+# }, os.path.join(relev, "test.pt"))
+# plot_dataset_mapping(os.path.join(relev, "test.pt"), f"NNICF Real\n{params}")
 
 end = time.time()
 # ~(480 - 830)s
-print(f'\nTotal execution time: {int((end - start) // 60)}:{(end - start) % 60:.1f} minutes')
+print(f'\nTotal execution time: {int((end - start) // 60)}:{(end - start) % 60:.2f} minutes')
 print()
-print(f"time taken in the icf calculations: {int(icf_time // 60)}:{icf_time % 60:.1f} minutes")
-print(f"time taken in middle samples_per_L loop: {int(samples_per_L_minus_icf_time // 60)}:{samples_per_L_minus_icf_time % 60:.1f} minutes")
-print(f"time taken in outer datasize loop: {int(datasize_minus_samples_per_L_loop // 60)}:{datasize_minus_samples_per_L_loop % 60:.1f} minutes")
-print(f"time taken in plotting: {end - t6:.2f} seconds")
+print(f"time taken in the icf calculations: {int(icf_time // 60)}:{icf_time % 60:.2f} minutes")
+print(f"time taken in middle samples_per_L loop: {int(samples_per_L_minus_icf_time // 60)}:{samples_per_L_minus_icf_time % 60:.2f} minutes")
+print(f"time taken in outer data_size loop: {int(data_size_minus_samples_per_L_loop // 60)}:{data_size_minus_samples_per_L_loop % 60:.2f} minutes")
+print(f"time taken in plotting: {end - t7:.2f} seconds")
+# print(f"time taken in plotting: {end - t6:.2f} seconds")

nnscf.py
@@ -13,25 +13,17 @@ device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
 # todo: see how can i use loss function to reduce ber
 criterion = nn.MSELoss()
 
-# ! instead of returning only real or imag part of two datasets (tx and rx), returns only one part of one dataset
-# todo merge it with renormalization notebook
-def normalize(X_raw):
-    X_batch = torch.tensor(X_raw, dtype=torch.float32)
+# ! instead of returning only two parts (real and imag) of two datasets (tx and rx), returns only one part of one dataset
+#%%
+def normalize(raw, limits):
+    min, max = limits
+    norm = 2.0 * ((raw - min) / (max - min)) - 1.0 if max != min else raw
+    return norm
 
-    # Convert PyTorch 0-D tensors to standard Python floats
-    X_min, X_max = X_batch.min().item(), X_batch.max().item()
-
-    X_norm = 2.0 * ((X_batch - X_min) / (X_max - X_min)) - 1.0 if X_max != X_min else X_batch
-
-    # 3. Return as a hyper-fast PyTorch file
-    return X_norm, (X_min, X_max)
-
-# ! should be used on real and imag of each entry in the dataset separately
-# ! to normalize the whole dataset using global values, run renormalization.ipynb after this function
-def denormalize(pred_norm, minmax):
-    raw_min, raw_max = minmax
-    pred_raw = ((pred_norm + 1.0) / 2.0) * (raw_max - raw_min) + raw_min if raw_max != raw_min else pred_norm
-    return pred_raw
+def denormalize(norm, limits):
+    min, max = limits
+    raw = ((norm + 1.0) / 2.0) * (max - min) + min if max != min else norm
+    return raw
 
 class TriangularActivation(nn.Module):
     """

nnscftest.py
@@ -1,6 +1,6 @@
 import numpy as np
 from ofdm.metrics import calculate_papr, calculate_cm
-from ofdm.plots import plot_ccdf, plot_ccdf_compare, plot_signals
+from ofdm.plots import plot_ccdf, plot_ccdf_compare
 import torch
 from torch.utils.data import Dataset, DataLoader, Subset
 from nnscf import NNSCFMapper, device, denormalize, criterion
@@ -24,6 +24,56 @@ mod = "16qam"
 # mod = "qpsk"
 print(f"Using {mod.upper()} modulation technique")
 
+# Clipping technique
+# tech = "icf"
+tech = "scf"
+print(f"Using {tech} clipping technique")
+
+# Hyperparameters
+epochs = 100
+# using 0.001 for Adam and 0.01 for LBFGS as default values
+# lr = 0.001
+lr = 0.1
+lr_list = str(float(lr)).split(".")
+lr_str = f"dot{lr_list[1]}" if lr < 1 else f"{lr_list[0]}dot{lr_list[1]}"
+print(f"Hyperparameters: epochs = {epochs}, learning rate = {lr} ({lr_str} used in naming files), batch size = None (for now)")
+
+# Optimizer
+# opt = "Adam"
+opt = "LBFGS"
+print(f"Using {opt} optimizer")
+
+# Data splitting
+# ! make sure that the splitting is the same as in renormalization.ipynb
+data_size = 100
+train_size = 70
+val_size = 10
+test_size = data_size - train_size - val_size
+if test_size:
+    one_batch = None
+else:
+    test_size = 1
+    one_batch = "00"
+train_val_test_str = f"{train_size}_{val_size}_{test_size}" if val_size else f"{train_size}_{test_size}"
+print(f"dataset of size {data_size} is split into {train_size} training files, and {val_size} validation and {test_size} testing batches respectively ({train_val_test_str} used in naming files)")
+
+batch_suffix = ""
+if one_batch:
+    batch_suffix = f"Batch #{one_batch}"
+    print(f"This one batch is of index {one_batch}")
+    if train_size + val_size == data_size:
+        print(batch_suffix, "was already used in training")
+
+params = f"{opt} optimizer {tech.upper()} {mod.upper()} lr = {lr}\n({train_size} Training/{val_size} Validation/{test_size} Testing){" (" + batch_suffix + ")" if batch_suffix else ''}"
+print(params)
+
+samples_per_L = 10000
+
+# Modulation scheme
+mod = "16qam"
+# mod = "qpsk"
+print(f"Using {mod.upper()} modulation technique")
+
 # clipping technique
 # tech = "icf"
 tech = "scf"
@@ -32,52 +82,50 @@ print(f"Using {tech} clipping technique")
 # Hyperparameters
 epochs = 100
 # using 0.001 for Adam and 0.01 for LBFGS as default values
-lr = 0.001
-# lr = 0.01
+# lr = 0.001
+lr = 0.1
 lr_list = str(float(lr)).split(".")
 lr_str = f"dot{lr_list[1]}" if lr < 1 else f"{lr_list[0]}dot{lr_list[1]}"
 print(f"Hyperparameters: epochs = {epochs}, learning rate = {lr} ({lr_str} used in naming files), batch size = None (for now)")
 
 # Optimizer
-opt = "Adam"
-# opt = "LBFGS"
+# opt = "Adam"
+opt = "LBFGS"
 print(f"Using {opt} optimizer")
 
 # Data splitting
 # ! make sure that the splitting is the same as in renormalization.ipynb
-datasize = 100
-# datasize = 120
+data_size = 100
 train_size = 70
 val_size = 10
-test_size = datasize - train_size - val_size
+test_size = data_size - train_size - val_size
 if test_size:
     one_batch = None
 else:
     test_size = 1
     one_batch = "00"
 train_val_test_str = f"{train_size}_{val_size}_{test_size}" if val_size else f"{train_size}_{test_size}"
-print(f"dataset of size {datasize} is split into {train_size} training files, and {val_size} validation and {test_size} testing batches respectively ({train_val_test_str} used in naming files)")
+print(f"dataset of size {data_size} is split into {train_size} training files, and {val_size} validation and {test_size} testing batches respectively ({train_val_test_str} used in naming files)")
 
 batch_suffix = ""
 if one_batch:
     batch_suffix = f"Batch #{one_batch}"
     print(f"This one batch is of index {one_batch}")
-    if train_size + val_size == datasize:
+    if train_size + val_size == data_size:
         print(batch_suffix, "was already used in training")
 
 params = f"{opt} optimizer {tech.upper()} {mod.upper()} lr = {lr}\n({train_size} Training/{val_size} Validation/{test_size} Testing){" (" + batch_suffix + ")" if batch_suffix else ''}"
 print(params)
 
-og_pt_dir = "./pt_dir/"
-pt_dir = "./pt_dir_globalnorm/"
-model_dir = "./new architecture/"
-graph_dir = "./new architecture/"
-os.makedirs(og_pt_dir, exist_ok=True)
-os.makedirs(pt_dir, exist_ok=True)
-os.makedirs(model_dir, exist_ok=True)
-os.makedirs(graph_dir, exist_ok=True)
-print(og_pt_dir, "is the location of the original pt files before implementing the brand new normalization method suggested by gemini")
-print(f'"{pt_dir}", "{model_dir}", and "{graph_dir}" are the 3 locations for pt files, nn model weights and graphs respectively')
+raw_dir = "./raw dir/"
+norm_dir = "./norm dir/"
+relev = "./relevant files/"
+os.makedirs(raw_dir, exist_ok=True)
+os.makedirs(norm_dir, exist_ok=True)
+os.makedirs(relev, exist_ok=True)
+print(raw_dir, "is the location of the raw unnormalized data files")
+print(norm_dir, "is the location of the normalized data files")
+print(relev, "is the location of other important files such as checkpoints and PNGs")
 
 # -----------------------------------------------------------------------------
 cell3 = time.time()
@@ -88,11 +136,11 @@ class FastOFDMDataset(Dataset):
         self.part = part
 
     def __len__(self):
-        return datasize # Assuming exactly 100 pre-computed .pt files, regardless of train/test split
+        return data_size # Assuming exactly data_size pre-computed .pt files, regardless of train/test split
 
     def __getitem__(self, idx):
         # Instantly loads the pre-normalized tensors directly into memory!
-        file_path = os.path.join(self.folder_path, f"{mod}_{tech}_part_{idx:03d}_{self.part}.pt")
+        file_path = os.path.join(self.folder_path, f"{mod}_{tech}_part_{idx:02d}_{self.part}.pt")
         # Load the dictionary
         data_pkg = torch.load(file_path, weights_only=False)
 
@@ -102,26 +150,51 @@ class FastOFDMDataset(Dataset):
                 data_pkg['Y_min'], data_pkg['Y_max'])
 
 # -----------------------------------------------------------------------------
-cell9 = time.time()
+cell6 = time.time()
+
+dataset_real = FastOFDMDataset(norm_dir, part='real')
+dataset_imag = FastOFDMDataset(norm_dir, part='imag')
+
+# -----------------------------------------------------------------------------
+cell10 = time.time()
+
+# ! this part is actually unnecessary in the notebook as the checkpoints are already loaded
+checkpoint_re = torch.load(
+    os.path.join(relev, f"{opt}_{mod}_{tech}_weights_limits_re_{train_val_test_str}_{lr_str}.pth"),
+    weights_only=False
+)
+checkpoint_im = torch.load(
+    os.path.join(relev, f"{opt}_{mod}_{tech}_weights_limits_im_{train_val_test_str}_{lr_str}.pth"),
+    weights_only=False
+)
 
 # 1. Load the Saved Models
 Test_Mod_Re = NNSCFMapper().to(device)
 Test_Mod_Im = NNSCFMapper().to(device)
 
-Test_Mod_Re.load_state_dict(torch.load(os.path.join(model_dir, f"{opt}_{mod}_{tech}_mod_re_weights_{train_val_test_str}_{lr_str}.pth"), weights_only=True))
-Test_Mod_Im.load_state_dict(torch.load(os.path.join(model_dir, f"{opt}_{mod}_{tech}_mod_im_weights_{train_val_test_str}_{lr_str}.pth"), weights_only=True))
+Test_Mod_Re.load_state_dict(checkpoint_re['model_state_dict'])
+Test_Mod_Im.load_state_dict(checkpoint_im['model_state_dict'])
 
 Test_Mod_Re.eval()
 Test_Mod_Im.eval()
 
+# 2. Grab the saved normalization limits
+# X_r_min = checkpoint_re['X_min']
+# X_r_max = checkpoint_re['X_max']
+Y_r_min = checkpoint_re['Y_min']
+Y_r_max = checkpoint_re['Y_max']
+
+# X_i_min = checkpoint_im['X_min']
+# X_i_max = checkpoint_im['X_max']
+Y_i_min = checkpoint_im['Y_min']
+Y_i_max = checkpoint_im['Y_max']
+
 # -----------------------------------------------------------------------------
 
-dataset_real = FastOFDMDataset(pt_dir, part='real')
-dataset_imag = FastOFDMDataset(pt_dir, part='imag')
 if one_batch:
     # 2. Grab one batch of data to test (Load the dictionary packages)
-    pkg_real = torch.load(os.path.join(pt_dir, f"{mod}_{tech}_part_{one_batch}_real.pt"), weights_only=False)
-    pkg_imag = torch.load(os.path.join(pt_dir, f"{mod}_{tech}_part_{one_batch}_imag.pt"), weights_only=False)
+    pkg_real = torch.load(os.path.join(norm_dir, f"{mod}_{tech}_part_{one_batch}_real.pt"), weights_only=False)
+    pkg_imag = torch.load(os.path.join(norm_dir, f"{mod}_{tech}_part_{one_batch}_imag.pt"), weights_only=False)
     # Extract tensors
     test_X_real, test_Y_real = pkg_real['X_norm'], pkg_real['Y_norm']
     test_X_imag, test_Y_imag = pkg_imag['X_norm'], pkg_imag['Y_norm']
@@ -136,20 +209,20 @@ if one_batch:
         predicted_imag = Test_Mod_Im(X_imag_flat).view_as(test_X_imag).cpu().numpy()
 
     # Denormalize using exact dictionary limits
-    pred_denorm_real = denormalize(predicted_real, (pkg_real['X_min'], pkg_real['X_max']))
-    pred_denorm_imag = denormalize(predicted_imag, (pkg_imag['X_min'], pkg_imag['X_max']))
+    # ? which limits should i use when using one_batch, local or global?
+    # pred_denorm_real = denormalize(predicted_real, (pkg_real['Y_min'], pkg_real['Y_max']))
+    # pred_denorm_imag = denormalize(predicted_imag, (pkg_imag['Y_min'], pkg_imag['Y_max']))
+    pred_denorm_real = denormalize(predicted_real, (Y_r_min, Y_r_max))
+    pred_denorm_imag = denormalize(predicted_imag, (Y_i_min, Y_i_max))
 
     # 4. Reconstruct the Complex OFDM Signals
     original_complex = test_X_real.numpy() + 1j * test_X_imag.numpy()
     clipped_complex = test_Y_real.numpy() + 1j * test_Y_imag.numpy()
     predicted_complex = pred_denorm_real + 1j * pred_denorm_imag
 else:
-#     # 2. Setup the Test Data (Files train_size->99)
-#     test_dataset_real = Subset(FastOFDMDataset(pt_dir, part='real'), range(train_size, datasize))
-#     test_dataset_imag = Subset(FastOFDMDataset(pt_dir, part='imag'), range(train_size, datasize))
-    # 2. Setup the Test Data (Files (train_size+val_size)->99)
-    test_dataset_real = Subset(dataset_real, range(train_size + val_size, datasize))
-    test_dataset_imag = Subset(dataset_imag, range(train_size + val_size, datasize))
+    # 2. Setup the Test Data (Files (train_size + val_size) -> data_size - 1)
+    test_dataset_real = Subset(dataset_real, range(train_size + val_size, data_size))
+    test_dataset_imag = Subset(dataset_imag, range(train_size + val_size, data_size))
 
     # We set shuffle=False to ensure real and imag batches stay perfectly aligned
     test_loader_real = DataLoader(test_dataset_real, batch_size=None, shuffle=False)
@@ -161,7 +234,7 @@ else:
     test_loss_real = 0.0
     test_loss_imag = 0.0
     with torch.no_grad():
-        for (X_real, Y_real, X_r_min, X_r_max, _, _), (X_imag, Y_imag, X_i_min, X_i_max, _, _) in zip(test_loader_real, test_loader_imag):
+        for (X_real, Y_real, _, _, _, _), (X_imag, Y_imag, _, _, _, _) in zip(test_loader_real, test_loader_imag):
             # Move inputs to device
             X_real, X_imag = X_real.to(device), X_imag.to(device)
 
@@ -186,8 +259,8 @@ else:
             pred_imag = pred_imag_flat.view_as(X_imag).cpu().numpy()
 
             # 4. Denormalize using the exact physical limits from the dictionary!
-            pred_denorm_real = denormalize(pred_real, (X_r_min, X_r_max))
-            pred_denorm_imag = denormalize(pred_imag, (X_i_min, X_i_max))
+            pred_denorm_real = denormalize(pred_real, (Y_r_min, Y_r_max))
+            pred_denorm_imag = denormalize(pred_imag, (Y_i_min, Y_i_max))
 
             # 5. Reconstruct complex signals
             orig_complex = X_real.cpu().numpy() + 1j * X_imag.cpu().numpy()
@@ -214,10 +287,10 @@ else:
     print(f'Testing complete! Aggregated {test_size * samples_per_L} OFDM symbols.')
 
 # -----------------------------------------------------------------------------
-cell10 = time.time()
+cell11 = time.time()
 
 # 5. Calculate PAPR and CM for CCDF
-orig_papr, clip_papr, pred_papr = [], [], []
+# orig_papr, clip_papr, pred_papr = [], [], []
 orig_cm, clip_cm, pred_cm = [], [], []
 
 # Iterate through the arrays
@@ -230,13 +303,13 @@ for i in range(test_size * samples_per_L if not one_batch else samples_per_L):
     clip_cm.append(calculate_cm(clipped_complex[i]))
     pred_cm.append(calculate_cm(predicted_complex[i]))
 
-orig_papr, clip_papr, pred_papr = np.array(orig_papr), np.array(clip_papr), np.array(pred_papr)
+# orig_papr, clip_papr, pred_papr = np.array(orig_papr), np.array(clip_papr), np.array(pred_papr)
 orig_cm, clip_cm, pred_cm = np.array(orig_cm), np.array(clip_cm), np.array(pred_cm)
 
 # 6. Plot the CCDF
 title = f'NN{tech.upper()} Predicted OFDM\n{params}'
 labels = ['Original OFDM', f'{tech.upper()}', f'NN{tech.upper()} Predicted OFDM']
-papr_list = [orig_papr, clip_papr, pred_papr]
+# papr_list = [orig_papr, clip_papr, pred_papr]
 cm_list = [orig_cm, clip_cm, pred_cm]
 
 # plot_ccdf_compare(papr_list, f'Original vs {tech.upper()} vs {title}', labels)
@@ -246,11 +319,11 @@ plot_ccdf_compare(cm_list, f'Original vs {tech.upper()} vs {title}', labels, met
 
 # todo: find a way to embed the floor part into the plotting function
 # 1. Define the target y-levels (probabilities)
-papr_target_y = 1e-4
+# papr_target_y = 1e-4
 cm_target_y = 1e-3
 
 # 2. Convert CCDF y-level to a standard percentile (e.g., 1e-4 becomes 99.99)
-papr_percentile = (1.0 - papr_target_y) * 100.0
+# papr_percentile = (1.0 - papr_target_y) * 100.0
 cm_percentile = (1.0 - cm_target_y) * 100.0
 
 # 3. Extract the exact x-axis values (PAPR/CM) where the line cuts the graph
@@ -274,11 +347,10 @@ cm_vlines = [
 # ]
 
 # # 4. Plot!
-# # papr_image_path = os.path.join(graph_dir, f"{opt}_{mod}_papr_{train_val_test_str}_{lr_str}.png")
-# # plot_ccdf(pred_papr, title, metric="papr", vlines=papr_vlines, save=papr_image_path)
+# # papr_image_path = os.path.join(relev, f"{opt}_{mod}_{train_val_test_str}_{lr_str}_papr.png")
 # plot_ccdf(pred_papr, title, metric="papr", vlines=papr_vlines)
 
-# cm_image_path = os.path.join(graph_dir, f"{opt}_{mod}_cm_{train_val_test_str}_{lr_str}.png")
+# cm_image_path = os.path.join(relev, f"{opt}_{mod}_{tech}_{train_val_test_str}_{lr_str}_cm.png")
 plot_ccdf(pred_cm, title, metric="cm", vlines=cm_vlines)
 
 # -----------------------------------------------------------------------------
@@ -286,8 +358,8 @@ end = time.time()
 # ~98s at test_size = 20
 # ~6s at one_batch != None
 print(f"\nTotal execution time: {end - start:.2f} seconds")
-print(f"Custom Activation Function (Cell 2) time: {cell3 - start:.2f} seconds")
-print(f"Highly Optimized Dataset Class (Cell 3) time: {cell9 - cell3:.2f} seconds")
-print(f"Testing (Cell 9) time: {cell10 - cell9:.2f} seconds")
-print(f"Plotting (Cell 10) time: {end - cell10:.2f} seconds")
-
+print(f"Imports and Device Setup (Cell 1) time: {cell3 - start:.2f} seconds")
+print(f"Highly Optimized Dataset Class (Cell 3) time: {cell6 - cell3:.2f} seconds")
+print(f"Initialization and DataLoaders (Cell 6) time: {cell10 - cell6:.2f} seconds")
+print(f"Testing (Cell 10) time: {cell11 - cell10:.2f} seconds")
+print(f"Plotting (Cell 11) time: {end - cell11:.2f} seconds")